e38b3ad72c90c5ac422ba631e649eb2b19fceb9e
adam-phillipps <adam.phillipps@gmail.com>
2015-07-20 22:41:19 -0600

    First Commit

---
+++ spec/spot_maker_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+require '/Users/adam/code/renderman/spot_maker'
+
+describe SpotMaker do
+  it 'is named Ash'
+end
---
+++ spot_maker.rb
@@ -0,0 +1,9 @@
+#require 'aws-sdk', '~> 2'
+
+class SpotMaker
+  def initialize
+    puts 'wtf'
+  end
+end
+
+SpotMaker.new

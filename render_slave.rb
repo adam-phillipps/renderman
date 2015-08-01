0064d862b0bbb291cfb9a531f2d894947d41829e
adam-phillipps <adam.phillipps@gmail.com>
2015-08-01 00:00:16 -0600

    WIP make ready for testing

--- render_slave.rb
+++ render_slave.rb
@@ -1,39 +1,50 @@
 require 'dotenv'
 Dotenv.load
 require 'aws-sdk'
-require 'spot_helper'
+require "#{File.dirname(__FILE__)}/spot_helper"
 require 'byebug'
 
 class RenderSlave
 	def initialize
 		@boot_time = Time.now.to_i # seconds from epoch
+		@backlog_queue_url = 'https://sqs.us-west-2.amazonaws.com/828660616807/backlog'
+		@wip_queue_url = 'https://sqs.us-west-2.amazonaws.com/828660616807/wip'
 		@sqs_client = SpotHelper.sqs_client
 		@ec2_client = SpotHelper.ec2_client
-		@poller = SpotHelper.sqs_poller
-		@instance_id = SpotHelper.get_self_instance_id
-		@backlog_queue_url = 'https://sqs.us-west-2.amazonaws.com/828660616807/backlog'
-		@initial_timeout = 3480 # 58 minutes
-		@message_receipt_handle = nil
+		@poller = SpotHelper.sqs_poller(@backlog_queue_url)
+#		@instance_id = SpotHelper.get_self_instance_id
+		@timeout = 120#3300 # 55 minutes
+		@initial_timeout = @timeout
+		@message = nil
 
-		@poller.poll(visibility_timeout: @initial_timeout) do |msg|
-			unless msg.receipt_handle.nil?
-				@message_receipt_handle = msg.receipt_handle 
-				run_job # run_job deletes message after it's finished
-				continue_to_poll if viable?
+		@poller.poll(visibility_timeout: @timeout) do |msg|			
+			@message = msg 
+			run_job # run_job deletes message after it's finished
+			if viable?
+				continue_to_poll
+			else
+				puts 'asdfasdfasdfasdfasdfasdf' if should_stop?#@ec2_client.stop_instances({instance_id: [@instance_id]}) if should_stop?
 			end
-			@ec2_client.stop_instances({[instance_id: @instance_id]}) if should_stop?
 		end
 	end
 
 	def run_job
+		send_message_to_wip
 		add_time_to_message_if_needed
+		# run the job and monitor the time so it can add more if it needs it
+		@sqs_client.delete_message({
+			queue_url: @backlog_queue_url,
+			receipt_handle: @message.receipt_handle})
+		@message = nil
+	end
 
-		@message_receipt_handle = nil
+	def job_still_running?
+		true
 	end
 
 	def should_stop?
 		if (ratio <= 10)
-			if ((@boot_time - Time.now.to_i) <= 120)
+			if ((@boot_time - Time.now.to_i) >= @timeout)
 				true
 			else
 				continue_to_poll
@@ -45,19 +56,14 @@ class RenderSlave
 	end
 
 	def continue_to_poll
-		unless @message_receipt_handle.nil?
-			@sqs_client.delete_message({
-				queue_url: @backlog_queue_url,
-				receipt_handle: @message_receipt_handle})
-		end
 	end
 
 	def add_time_to_message_if_needed
-		if (((@boot_time - Time.now.to_i) <= 300) && (job_still_running?))
+		if (((Time.now.to_i - @boot_time) >= @timeout) && (job_still_running?))
 			SpotHelper.sqs_client.change_message_visibility({
 				queue_url: @backlog_queue_url,
-				receipt_handle: @message_receipt_handle,
-				visibility_timeout: (@initial_timeout += 3480)})
+				receipt_handle: @message.receipt_handle,
+				visibility_timeout: (@initial_timeout += @timeout)})
 		end
 	end
 
@@ -67,6 +73,22 @@ class RenderSlave
 
 	def ratio
 		SpotHelper.ratio_of_backlog_to_wip
+	end
+
+	def send_message_to_wip	
+		if !@message.message_attributes.nil?
+			attributes = {
+			  "String" => {
+		      string_value: @message.message_attributes['string_value'],
+		      string_list_values: ["String"],
+		      data_type: "String"}}
+		else
+			attributes = {}
+		end
+		@sqs_client.send_message({
+		  queue_url: @wip_queue_url,
+		  message_body: @message.body,
+		  message_attributes: attributes})
 	end
 end
 
--- spot_helper.rb
+++ spot_helper.rb
@@ -18,7 +18,6 @@ module SpotHelper
   end
 
 	def SpotHelper.add_buffer_to_price(price)
-		byebug
 		new_price = price.to_f
   	((new_price + (new_price * 0.2)).round(3)).to_s
   end
@@ -29,7 +28,7 @@ module SpotHelper
   end
 
   def SpotHelper.number_of_jobs_in(url)
-  	@sqs.get_queue_attributes(
+  	sqs_client.get_queue_attributes(
   		queue_url: url,
   		attribute_names: ['ApproximateNumberOfMessages']).
   			attributes['ApproximateNumberOfMessages'].to_f
